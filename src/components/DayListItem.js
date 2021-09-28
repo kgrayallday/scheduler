0923e0a12d6b2af9a5d2c35ac480960a6ce539d2
Kyle Gray <kylemaxgray@gmail.com>
2021-09-28 15:47:43 -0700

    add test for button, create DayListItem and stories

---
+++ src/components/DayListItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  return (
+    <li>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
