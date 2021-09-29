eb0b8112b8f84fea45fee00f86c58663badf1ccb
Kyle Gray <kylemaxgray@gmail.com>
2021-09-28 18:46:18 -0700

    update li onclick and props

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,9 +1,11 @@
 import React from "react";
 import classNames from "classnames";
+import './DayListItem.scss';
 
 export default function DayListItem(props) {
+  let dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0})
   return (
-    <li>
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
