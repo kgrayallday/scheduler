9f5f7d4bd0693cdd2ee838d5973d4c24e475bfa8
Kyle Gray <kylemaxgray@gmail.com>
2021-09-30 08:36:35 -0700

    refactor to move onclick from InterviewerListItem to Interviewerlist to eliminate duplicate code

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,16 @@
 import React from "react";
 import './InterviewerListItem.scss';
-import classNames from "classnames";
 
 function InterviewerListItem(props) {
 
   return (
-    <li className={props.selected ? 'interviewers__item--selected' : 'interviewers__item'}>
+    <li className={props.selected ? 'interviewers__item--selected' : 'interviewers__item'} onClick={props.setInterviewer}>
       <img
         className="interviewers__item-image"
         src={props.avatar}
         alt={props.name}
       />
-      {props.selected ? props.name : ''}
+      {props.selected && props.name}
     </li>
   );
 }
