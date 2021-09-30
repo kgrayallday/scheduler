a2f812c0fa3b202411bc4c57c67e722db5e76e50
Kyle Gray <kylemaxgray@gmail.com>
2021-09-30 12:35:39 -0700

    add onClick for props.onAdd

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Empty() {
+function Empty(props) {
   return(
     <main className="appointment__add">
       <img
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
+        onClick={props.onAdd}
       />
     </main>
   );
