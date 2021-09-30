7cfbe9669e6875af96e7243d4e6a7b3cc67fb16b
Kyle Gray <kylemaxgray@gmail.com>
2021-09-30 09:17:30 -0700

    add Appointment stories for storybook

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,7 @@ import DayListItem from 'components/DayListItem';
 import DayList from 'components/DayList';
 import InterviewerListItem from 'components/InterviewerListItem';
 import InterviewerList from 'components/InterviewerList';
+import Appointment from 'components/Appointment/index.js'
 
 storiesOf("Button", module)
   .addParameters({
@@ -125,3 +126,10 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
+
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment", () => <Appointment time='12pm' /> );
