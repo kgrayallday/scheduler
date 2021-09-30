47b335f9d368041fd2bd41f44d3f2c0463cd38f6
Kyle Gray <kylemaxgray@gmail.com>
2021-09-30 12:22:28 -0700

    create basic Empty component

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Empty() {
+  return(
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+      />
+    </main>
+  );
+}
+
+export default Empty;
