cc122c7640cf18a1213a06d1349705fb34c652c1
Kyle Gray <kylemaxgray@gmail.com>
2021-09-30 12:31:06 -0700

    remove hard code time string replace with props.time

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Header() {
+function Header(props) {
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{props.time}</h4>
       <hr className="appointment__separator" />
     </header>
   );
