c78cb3ec0e53d4695cfdb9cce0a2313593a0e6b2
Kyle Gray <kylemaxgray@gmail.com>
2021-09-30 08:46:06 -0700

    update stories for storybook

--- stories/index.js
+++ stories/index.js
@@ -96,7 +96,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={action("setInterviewer")}
+      setInterviewer={event => action("setInterviewer")(interviewer.id)}
     />
   ));
 
