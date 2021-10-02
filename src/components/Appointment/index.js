372ca2427f7c3018a24c6da85ec144337e828357
Kyle Gray <kylemaxgray@gmail.com>
2021-10-01 18:34:22 -0700

    import Head, Show, Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import './styles.scss'
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
-function Appointment() {
+function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    </article>
   )
 }
 
