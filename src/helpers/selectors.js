5f4d672230eea210d681977d0a45e4b623c90e4c
Kyle Gray <kylemaxgray@gmail.com>
2021-10-03 13:41:25 -0700

    implement getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -15,3 +15,29 @@ export function getAppointmentsForDay(state, day) {
   return mappedAppointments;
 
 }
+
+export function getInterview(state, interview) {
+  // console.log('getInterview state >>> ', state);
+  // console.log('getInterview interview passed >>> ', interview);
+  // console.log('an appointments array >>> ', state.days[0].appointments)
+  // console.log('an appointments interview obj >>> ', state.appointments['3'].interview)
+  
+  if (!interview) {
+    return null;
+  }
+
+  // state contains { day{...}, appointments{...}, interviewers{...}}
+  // state.days contains {id, name, appointments[1, 2, 3]}
+  // state.appointments contains {key: {id, time, interview{...}}}
+  // state.interviewers contains {avatar, id, name}
+
+  // interview object contains { student: "name as string", interviewer: int ref interviewers id}
+
+  // needs to return obj of { student: "string name", interviewer: {} } for displaying in each of our appointments 
+  // given interview provide student + interview? 
+  // from the current state, get the interviewer id and return the interviewer obj
+
+  const getStateInterviewerFromId = state.interviewers[interview.interviewer];
+  return {interviewer: getStateInterviewerFromId, student:interview.student}
+
+}
