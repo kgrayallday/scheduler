d222d82154495f1ddd897d44a67c4ddac7b599fe
Kyle Gray <kylemaxgray@gmail.com>
2021-10-03 18:38:17 -0700

    add useVisualMode functionality in appointment index via conditionals in component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,28 @@ import './styles.scss'
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+
+
 
 function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (<Show student={props.interview.student} interviewer={props.interview.interviewer} />)}
+      {mode === CREATE && <Form interviewers={[]} onCancel={()=> back()}/>}
+
     </article>
   )
 }
