657255a119a8e8936ce8ceafa8f398dc18f37e66
Kyle Gray <kylemaxgray@gmail.com>
2021-10-04 15:38:54 -0700

    implement bookInterview and onSave fn to Form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -14,16 +14,25 @@ const CREATE = 'CREATE';
 
 function Appointment(props) {
 
+  // set our state variables
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview)
+  }
 
   return (
     <article className="appointment">
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (<Show student={props.interview.student} interviewer={props.interview.interviewer} />)}
-      {mode === CREATE && <Form interviewers={[]} onCancel={()=> back()}/>}
+      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={()=> back()} onSave={save} />}
 
     </article>
   )
